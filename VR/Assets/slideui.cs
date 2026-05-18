800ae2efaf9979a55be51bfcae3d4c89819ce730
Yuni <46707148+Yunirovski@users.noreply.github.com>
2026-05-18 03:45:04 +0200

    ui

---
+++ VR/Assets/slideui.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PressSpaceSlideUI : MonoBehaviour
+{
+    // --- UI position ---
+    public RectTransform uiPanel;
+    public Vector3 downPos;        
+    public Vector3 upPos;           
+
+    // --- Move speed ---
+    public float speed = 10f;
+
+    void Update()
+    {
+        if (uiPanel == null) return;
+
+        //  Space key
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Up
+            uiPanel.anchoredPosition = Vector3.Lerp(uiPanel.anchoredPosition, upPos, Time.deltaTime * speed);
+        }
+        else
+        {
+            // Down
+            uiPanel.anchoredPosition = Vector3.Lerp(uiPanel.anchoredPosition, downPos, Time.deltaTime * speed);
+        }
+    }
+}
