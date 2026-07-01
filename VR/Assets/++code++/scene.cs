00c2db57b9a082ae4cfc5a635c80fc0c0a25801e
Kotopuli <igorzuenko04@gmail.com>
2026-07-01 18:10:58 +0200

    fixes to build profiles and start up

--- VR/Assets/++code++/scene.cs
+++ VR/Assets/++code++/scene.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigation : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void LoadNextScene()
     {
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
